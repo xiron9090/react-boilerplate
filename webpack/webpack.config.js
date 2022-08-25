03a56a6707f005793fbc884b065cb57a45be8e9e
Livan Ramos Moreno <xiron9090@gmail.com>
2022-08-25 10:55:40 -0400

    Initialize feature/add-eslint

---
+++ webpack/webpack.config.js
@@ -0,0 +1,8 @@
+const {merge} = require('webpack-merge');
+const commonconfig = require('./webpack.common.js')
+module.exports = (envVars) => {
+  const {env} = envVars;
+  const envConfig = require(`./webpack.${env}.js`)
+  const config = merge(commonconfig, envConfig)
+  return config
+}
